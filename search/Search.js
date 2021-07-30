9b9aa331ed243f9c4d4b17089c7659603d1a28bf
Maurício Mussatto Scopel <mauricio.scopel@cwi.com.br>
2021-07-30 15:12:56 -0300

    Adjust to stop the search correctly.

--- search/Search.js
+++ search/Search.js
@@ -81,6 +81,9 @@ class Search {
                 message: "-- MORE --",
             },
         ]);
+        if (!answers.loadNextPage) {
+            this._projects = [];
+        }
 
         return answers.loadNextPage;
     }
